bfc22075c02f6527427d2a6d926d1fc506a67137
fikriwado <fixwad.online@gmail.com>
2024-03-27 13:49:58 +0700

    add toggle qrcode

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,11 @@
 import './scss/_home.scss'
 import { Layout } from '../../layouts'
 import { qrcode } from '../../assets'
+import { useState } from 'react'
 
 const Home = () => {
+  const [toggleQrCode, setToggleQrCode] = useState(false)
+
   return (
     <Layout>
       <section className='tech-saldo'>
@@ -12,7 +15,7 @@ const Home = () => {
           <div className='tech-saldo__detail'>
             <div className='tech-saldo__detail--left'>
               <div className='tech-saldo__left--qrcode'>
-                <img src={qrcode} alt='' />
+                <img src={qrcode} onClick={() => setToggleQrCode(!toggleQrCode)} alt='qrcode' />
               </div>
             </div>
             <div className='tech-saldo__detail--right'>
@@ -25,6 +28,15 @@ const Home = () => {
                 <span>2.500</span>
               </div>
             </div>
+          </div>
+        </div>
+        <div className={'tech-qrcode' + (toggleQrCode ? ' active' : '')}>
+          <div className='tech-qrcode__toggle--close' onClick={() => setToggleQrCode(!toggleQrCode)}>
+            &#x2715;
+          </div>
+          <div className='tech-qrcode__content'>
+            <p className='tech-qrcode__content--label'>Show the QR Code below to the cashier</p>
+            <img src={qrcode} className='tech-qrcode__content--img' alt='qrcode' />
           </div>
         </div>
       </section>
